ce75ef27ca18fb72203639314c515903b1877d92
Jon Schleuss <gaufre@gmail.com>
2016-11-15 11:54:11 -0800

    farts

--- 095-solano/results/parse_solano.py
+++ 095-solano/results/parse_solano.py
@@ -1,41 +1,70 @@
 import time
 
-files = ['prop51']
+files = ['prop52']
 
 allData = {}
 precincts = []
 
 for file in files:
-    f = open(file+'.txt','r').read()
-    j = open(file+'-cleaned.txt','w')
+    print file
+    f = open(file+'.txt','r')
+    lines = f.read().split('\n')
+    # j = open(file+'-cleaned.txt','w')
 
     # headers for new file
-    j.write("precinct,"+file+"_yes,"+file+"_no\n")
+    # j.write("precinct,"+file+"_yes,"+file+"_no\n")
 
-    lines = f.split('\n')
-    # newline = "" # starting string for lines once they're constructed
-    for line in lines:
-        cells = line.split('\t')
+    linelen = len(lines)
 
-        if "Precinct" in line:
-            # newline = ""
-            precinct = lines[lines.index(line)+1]
-            # newline += precinct + ','
+    for line in range(0,linelen,6):
+        precinct = lines[line+1]
 
-            if precinct not in precincts:
-                precincts.append(precinct)
-                allData[precinct] = {}
+        if precinct not in precincts:
+            precincts.append(precinct)
+            allData[precinct] = {}
 
-        elif " YES " in line:
-            # newline += cells[1] + ','
+        # check if prop in the file
+        if file not in allData[precinct]:
+            if " YES " in lines[line+3]:
+                yesvotes = lines[line+3].split('\t')[1]
+                novotes = lines[line+4].split('\t')[1]
+                allData[precinct][file] = {'yes':yesvotes,'no':novotes}
+            elif " NO " in lines[line+3]:
+                yesvotes = lines[line+4].split('\t')[1]
+                novotes = lines[line+3].split('\t')[1]
+                allData[precinct][file] = {'yes':yesvotes,'no':novotes}
+    f.close()
 
-        elif " NO " in line:
-            # newline += cells[1] + '\n'
+print allData
 
-        elif len(line) == 0:
-            # j.write(newline)
-            # newline = ""
-    j.close()
+        # print lines[line+1]
 
-        # print line
-        # time.sleep(1)
+    # # newline = "" # starting string for lines once they're constructed
+    # for line in lines:
+
+    #     cells = line.split('\t')
+
+    #     if "Precinct" in line:
+    #         index = lines.index(line)
+    #         precinct = lines[lines.index(line)+1]
+    #         # newline += precinct + ','
+    #         print index
+
+    #         if precinct not in precincts:
+    #             precincts.append(precinct)
+    #             allData[precinct] = {}
+
+    #     # elif " YES " in line:
+    #     #     # newline += cells[1] + ','
+
+    #     # elif " NO " in line:
+    #         # newline += cells[1] + '\n'
+
+    #     # elif len(line) == 0:
+    #         # j.write(newline)
+    #         # newline = ""
+    # j.close()
+    # print allData
+
+    #     # print line
+    #     # time.sleep(1)
