e3e8f3f2f9d855eafa9dffbde6586a69e59e8e79
Joe Fox <joe.m.fox@gmail.com>
2016-11-16 15:56:18 -0800

    everything

--- 079-san-luis-obispo-county/results/parse_slo.py
+++ 079-san-luis-obispo-county/results/parse_slo.py
@@ -1,7 +1,8 @@
-import sys, os, re
+import sys
+import os
+import re
 from subprocess import call
-
-call(['pdftotext','-layout','127.0.0.1'])
+call(['pdftotext','-layout',sys.argv[1]])
 pages = []
 currentPage = ''
 pageNo = 0
@@ -9,39 +10,13 @@ if not os.path.exists('results/'):
     os.makedirs('results/')
 
 with open(sys.argv[1].replace('pdf','txt')) as file:
-	for line in file:
-		if re.match(r'()',line):
-			pages.append(currentPage)
-			pageNo += 1
-			print "page %s" % (pageNo)
-			currentPage = line
-		else:
-			currentPage += line
-	file.close()
-
-# precincts = [re.findall(r'((?<= - )\d{7}[^,]+)',line) for line in file]
-currentRace = ''
-out = None
-for page in pages:
-	race = re.findall(r'(?:Santa Barbara County.+\n.+\n.+\n.+\n.+\n)(.+)',page,flags=re.MULTILINE);
-	raceString = ''
-	if race:
-		raceString = re.sub(r' {2,}',' ',race[0]);
-		print raceString
-	else:
-		raceScring = currentRace
-	if currentRace != raceString:
-		if out != None:
-			out.close()
-		filename = 'results/%s.csv' % (re.sub(r'\W','',raceString))
-		out = open(filename,'w')
-		currentRace = raceString;
-		# if race:
-		# 	out.write('RACE: %s' % (currentRace))
-		# else:
-		# 	print "no match"
-
-	results = [re.findall(r'(\d{2}-\d{4})(?:(.*\n){3,}?)(?:  Total)(.+\n)',page,flags=re.MULTILINE)]
-	print results
+	filename = 'results/%s.csv' % sys.argv[1].replace('.pdf','')
+	out = open(filename,'w')
+	resultstring = re.sub(r'Statement of Votes Cast(.*\n){13,14}( *(Jurisdiction|Total))',r'\2',file.read(),flags=re.MULTILINE)
+	resultstring = re.sub(r'(?:\n\s{3,})(Total.+)',r'\n\1',resultstring,flags=re.MULTILINE)
+	# resultstring = re.sub(r'(?:\n\s{3,})(\w{3} \d{3}-\d{2}|MB\d{2})',r'\n\1',resultstring,flags=re.MULTILINE)
+	print resultstring
+	results = [re.findall(r'(?: *)(\w{3} \d{3}-\d{2}|MB\d{2})(?:(\n)(.*\n){2,3} *)(Total.+)',resultstring,flags=re.MULTILINE)]
+	# print results
 	for x in range(0,len(results[0])):
-		out.write(re.sub(r' {2,}',',',''.join(results[0][x])))
+		out.write('%s,%s\n' % (results[0][x][0],re.sub(r' +',',',''.join(results[0][x][len(results[0][x])-1]))))
